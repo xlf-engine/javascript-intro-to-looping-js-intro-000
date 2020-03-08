67a757ab5027d2533db3455bc50b8c673ab70c5d
NT <nicolasturek@gmail.com>
2020-03-08 02:42:48 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+let array = [];
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1){
+      console.log("I am 1 strange loop.")
+  } else {
+    console.log(`I am ${i} strange loop.`)
+  }
+}
