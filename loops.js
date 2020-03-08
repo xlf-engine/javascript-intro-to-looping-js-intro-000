5c98832b7862cfcb1d8b9c8fb88b7b4c0bc669a9
NT <nicolasturek@gmail.com>
2020-03-08 02:42:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
-let array = [];
-
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1){
-      console.log("I am 1 strange loop.")
+      array.push(`I am 1 strange loop.`)
   } else {
-    console.log(`I am ${i} strange loop.`)
+    array.push(`I am ${i} strange loop.`)
   }
+  return array;
 }
